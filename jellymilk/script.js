7b3996364dcb049e2f38bf169f0c491086dd8a48
kashikichi <60090083+Pikselas@users.noreply.github.com>
2022-06-19 18:42:31 +0530

    Test Profile

--- jellymilk/script.js
+++ jellymilk/script.js
@@ -5,20 +5,32 @@ function ToggleContainer()
 
 async function GetModels(modelType)
 {
-    let res = await fetch(`https://pikselas.github.io/jellymilk/data/${modelType}.json`);
+    let res = await fetch(`data/${modelType}.json`);
     if(res.ok)
     {
         return await res.json();
     }
 }
 
+function CreateModelPanel(name)
+{
+    let panel = document.createElement("div");
+    let pc = document.createElement("img");
+    pc.src = pic;
+    let title = document.createElement("h2");
+    title.innerHTML = "profile_pics/" + name + ".png";
+    panel.appendChild(pc);
+    panel.appendChild(title);
+    return panel;
+}
+
 function ShowModels(modelType)
 {
     let Modcon = document.getElementById("ModelsContainer").children[1];
     Modcon.innerHTML = "";
-    GetModels(modelType).then((models)=>{
-        Object.keys(models).forEach((model)=>{
-            console.log(model);
+    GetModels(modelType).then((modelsJson)=>{
+        modelsJson["models"].forEach(model => {
+            Modcon.appendChild(CreateModelPanel(model));            
         });
-    })
+    });
 }
