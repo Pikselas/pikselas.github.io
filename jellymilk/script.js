b6bd319bf463607887e498c56bfe3d418602ec32
kashikichi <60090083+Pikselas@users.noreply.github.com>
2022-06-19 16:34:10 +0530

    🤔🤔

--- jellymilk/script.js
+++ jellymilk/script.js
@@ -3,11 +3,22 @@ function ToggleContainer()
     document.getElementById("ModelsContainer").hidden ^= 1;
 }
 
+async function GetModels(modelType)
+{
+    let res = await fetch(`https://pikselas.github.io/jellymilk/data/${modelType}.json`);
+    if(res.ok)
+    {
+        return await res.json();
+    }
+}
+
 function ShowModels(modelType)
 {
-    fetch(`data/${modelType}.json`).then((res)=>{
-        res.json().then((data)=>{
-            console.log(data);
+    let Modcon = document.getElementById("ModelsContainer").children[1];
+    Modcon.innerHTML = "";
+    GetModels(modelType).then((models)=>{
+        Object.keys(models).forEach((model)=>{
+            console.log(model);
         });
-    });
+    })
 }
