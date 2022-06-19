bae09b190aebcc7bc63d6f6caa3348ca982902d0
kashikichi <60090083+Pikselas@users.noreply.github.com>
2022-06-19 15:43:55 +0530

    model test

--- jellymilk/script.js
+++ jellymilk/script.js
@@ -2,3 +2,12 @@ function ToggleContainer()
 {
     document.getElementById("ModelsContainer").hidden ^= 1;
 }
+
+function ShowModels(modelType)
+{
+    fetch(`data/${modelType}.json`).then((res)=>{
+        res.json().then((data)=>{
+            console.log(data);
+        });
+    });
+}
